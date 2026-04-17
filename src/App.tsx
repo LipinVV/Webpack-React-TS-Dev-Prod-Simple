72e1928bf7f9f3a70227a6afcdf7d1bcfa04bd0f
ultra <vit.lipin@gmail.com>
2026-04-18 01:15:39 +0300

    cleaned useless files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import React, {FC, useState} from 'react';
+import React, {FC} from 'react';
 
 const App: FC = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="app">
-      <h1>React 16 + Webpack</h1>
-      <p>Счетчик: {count}</p>
-      <button onClick={() => setCount(count + 1)}>
-        Нажми меня
-      </button>
+        Build is ready to use
     </div>
   );
 };
